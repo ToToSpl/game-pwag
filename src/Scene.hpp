67dc188143840373fca4c0fd984471a263003a79
Jacek Grzybowski <totospl99@gmail.com>
2022-11-18 02:38:51 +0100

    SceneObject concept

--- src/Scene.hpp
+++ src/Scene.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <GL/glew.h>
+#include <glm/glm.hpp>
 #include <map>
 #include <string>
 #include <vector>
@@ -11,15 +12,19 @@ namespace Game {
 
 using json = nlohmann::json;
 
+struct SceneObject {
+  glm::mat4 transform = glm::mat4(1);
+  float scale = 1.0;
+};
+
 struct BasicEntity {
   std::string name;
   float* vertecies;
   u_int8_t* indecies;
   GLuint vertArr, indArr;
+  std::vector<SceneObject*> objects;
 }; // this element holds vertex and index data about some shape.
    // It can be rerendered many times.
-
-struct SceneObject {};
 
 class Scene {
 private:
