004f3e0ec82be3cff580e692a56a71fed90a9a35
Jacek Grzybowski <totospl99@gmail.com>
2022-11-26 03:12:07 +0100

    changed constants

--- src/constants.h
+++ src/constants.h
@@ -8,11 +8,12 @@
 #define CUBE_VERT 6 * 4 * 3
 #define CUBE_IND 6 * 4 * 3
 #define CUBE_UV 6 * 4 * 2
+#define CUBE_NORMAL 6 * 4 * 3
 
 #define FOV 45.0f
 
-#define GRAVITY 0.0015f
+#define GRAVITY 0.0005f
 #define PLAYER_SPEED 0.00007f
 #define PLAYER_DRAG 0.4f
-#define PLAYER_JUMP 0.03f
+#define PLAYER_JUMP 0.01f
 #define MOUSE_SPEED 0.001f
